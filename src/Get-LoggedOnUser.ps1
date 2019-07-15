5916f9fbfced04a8343da8eabf6ab73f91573ed5
Lawrence Hwang <coffeepoweredlion@gmail.com>
2019-07-14 20:27:12 -0700

    Update the sample script

--- src/Get-LoggedOnUser.ps1
+++ src/Get-LoggedOnUser.ps1
@@ -6,53 +6,41 @@ function Get-LoggedOnUser {
         [ValidateNotNullOrEmpty()]
         [String[]]$ComputerName
     )
-    Process {
+    process {
         if ($null -eq $ComputerName) {
             $user = (Get-CimInstance -ClassName Win32_computersystem -Property username -ErrorAction Stop).UserName
-            $prop = @{
-                'Computer'     = $env:COMPUTERNAME
+            $obj = [PSCustomObject]@{
+                'ComputerName' = $env:COMPUTERNAME
                 'LoggedOnUser' = $user
             }
-            $obj = New-Object -TypeName PSObject -Property $prop
-            $obj
         }
         else {
-            Foreach ($c in $ComputerName) {
-                Try {
-                    $AllOK = $true
-
-                    if (Test-Connection -Quiet -ComputerName $c -count 1) {
+            foreach ($c in $ComputerName) {
+                try {
+                    if (Test-Connection -Quiet -ComputerName $c -count 1 -ErrorAction SilentlyContinue | Out-Null) {
                         Write-Verbose -message "Checking the logged on user on $c"
-                        $user = (Get-CimInstance -ClassName Win32_computersystem -Property username -ComputerName $c -ErrorAction Stop).UserName
+                        $CimSplat = @{
+                            ClassName = 'Win32_ComputerSystem'
+                            Property = 'username'
+                            ComputerName = $c
+                            ErrorAction = 'STOP'
+                        }
+                        $user = (Get-CimInstance @CimSplat).UserName
                     }
                     else {
-                        Write-Verbose -Message "Computer $c is not on the network."
-                        $emsg = $c
-                        Throw "Can't contact $emsg"
+                        Write-Warning -Message "Computer $c is not reachable on the network or permission denied."
+                        throw "Unable to access $c"
                     }
                 }
-                Catch {
-                    $AllOK = $false
-
-                    $prop = @{
-                        'Computer'     = $c;
+                catch {
+                    $obj = [PSCustomObject]@{
+                        'ComputerName' = $c
                         'LoggedOnUser' = 'N/A'
                     }
-
-                    Write-Verbose -message "Problem with fetching logged on user info."
-                    $obj = New-Object -TypeName PSObject -Property $prop
-                    Write-Output $obj
-
-                }
-                if ($AllOK) {
-                    $prop = @{
-                        'Computer'     = $c;
-                        'LoggedOnUser' = $user
-                    }
-                    $obj = New-Object -TypeName PSObject -Property $prop
-                    $obj
+                    Write-Verbose -message "Problem with fetching logged on user info on $c"
                 }
             }
         }
+        $obj
     }
 }
